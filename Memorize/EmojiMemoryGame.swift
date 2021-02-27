42447b17ecd69a683b19efed5b5f08525c37e8e7
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-02-27 14:08:51 -0300

    create a function to create memory game with random emojis on EmojiMemoryGame

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 class EmojiMemoryGame {
-    private(set) var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 2) { _ in "🥳" }
-    
+    private(set) var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+        
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis: Array<String> = ["👻", "🎃"]
+        return MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
     
     //MARK: - Access to the model
     var cards: Array<MemoryGame<String>.Card> {
