31da9ab87bcbe9da589ba9b5e37eafd147c09bba
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-02-27 12:47:16 -0300

    create a first view structure to memory game

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        HStack(content: {
+            ForEach(0..<4) {_ in
+                CardView(isFaceUp: true)
+            }
+        })
             .padding()
+            .font(Font.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack(content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 3.0).stroke(Color.orange)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+            }
+        })
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Felipe Guimarães on 27/02/21.
+//
+
+import Foundation
